7b29446ed9e4148a7441ae32e2f82476f61f7980
lindseynicolaysen11 <lindseynicolaysen11@augustana.edu>
2015-04-01 15:47:48 -0500

    Started layout development, added ButtonSequence class

---
+++ app/src/main/java/edu/augustana/csc490/memorybuttongame/ButtonSequence.java
@@ -0,0 +1,43 @@
+package edu.augustana.csc490.memorybuttongame;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+/**
+ * Created by lindseynicolaysen11 on 4/1/2015.
+ */
+public class ButtonSequence {
+    private ArrayList<Integer> seq;
+
+    public ButtonSequence() {
+        seq = new ArrayList<Integer>();
+        for (int i = 1; i <= 9; i++) {
+            seq.add(i);
+        }
+        randomize();
+    }
+
+    public ArrayList<Integer> getSeq() {
+        return seq;
+    }
+
+    public void randomize() {
+        Collections.shuffle(seq);
+    }
+
+    public String toString(){
+        return seq.toString();
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
--- app/src/main/java/edu/augustana/csc490/memorybuttongame/MainActivity.java
+++ app/src/main/java/edu/augustana/csc490/memorybuttongame/MainActivity.java
@@ -1,9 +1,14 @@
 package edu.augustana.csc490.memorybuttongame;
 
+import android.bluetooth.BluetoothClass;
+import android.graphics.Point;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +17,42 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get device width
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int device_width = size.x;
+        //create buttons
+        //set button width to fit any size screen
+        int button_width = device_width/3 - 30;
+        Button button1 = (Button) findViewById(R.id.button1);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+        Button button4 = (Button) findViewById(R.id.button4);
+        Button button5 = (Button) findViewById(R.id.button5);
+        Button button6 = (Button) findViewById(R.id.button6);
+        Button button7 = (Button) findViewById(R.id.button7);
+        Button button8 = (Button) findViewById(R.id.button8);
+        Button button9 = (Button) findViewById(R.id.button9);
+                button1.setWidth(button_width);
+        button2.setWidth(button_width);
+        button3.setWidth(button_width);
+        button4.setWidth(button_width);
+        button5.setWidth(button_width);
+        button6.setWidth(button_width);
+        button7.setWidth(button_width);
+        button8.setWidth(button_width);
+        button9.setWidth(button_width);
+
+
+//get and test sequence
+        ButtonSequence bSeq = new ButtonSequence();
+        String s = bSeq.toString();
+        Log.d("gameTag", s);
+
+
+
     }
 
 
